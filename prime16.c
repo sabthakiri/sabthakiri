1870ed58fde0674ba8ac972189362d4b48c5a7cd
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-01-21 07:02:05 -0800

    Create prime16.c

---
+++ prime16.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+int low, high, i, flag;
+printf("Enter numbers(intervals): ");
+scanf("%d %d", &low, &high);
+while (low <high)
+{
+flag = 0;
+for(i = 2; i <= low/2; ++i)
+{
+if(low % i == 0)
+{
+flag = 1;
+break;
+}
+++low;
+}
+return 0;
+}
