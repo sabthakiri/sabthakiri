a02493fd3af8f6563193154dc722b75de164b71b
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-01-31 15:58:04 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) 
+{
+int a,i;
+printf("enter the  sentance");
+scanf("%d",&a);
+if(count);
+{
+count=count+i;
+printf("%d enter the value of count",\n);
+}
+return 0;
+}
