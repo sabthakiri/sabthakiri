a3c565b8186e2fa8ccde46021dc3628731ae8efd
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-02-02 16:08:40 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) 
+{
+char a[1000];
+int i,count=1;
+printf("enter the sentence \n");
+scanf("%s",a[1000]);
+for(i=0;a[i]='\0';i++)
+{
+if(a[i]=='.');
+count++;
+}
+printf("the number of space in string is %d",count+1);
+return 0;
+}
