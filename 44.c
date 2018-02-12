eb41e57c8dad0c62a5c7b9a712b9dcfffab240ea
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-02-12 16:18:08 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+printf("enter the number \n");
+ scanf("%d",&i);
+if (i<10)
+{
+printf("yes \n",&i);
+scanf("%d",&i);
+}
+if (i>10)
+{
+printf("no \n",&i);
+scanf("%d",&i);
+}
+return 0;
+ }
