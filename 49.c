8b1b41d49388c81c20441414c407e1916ee47e04
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-03-02 16:30:25 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+{
+   int n, sum = 0,  a;
+ printf("Enter the number \n");
+   scanf("%d", &n);
+ printf("Enter %d integers\n",n);
+ 
+   {
+      scanf("%d", &a);
+      sum = sum ++;
+   }
+ 
+   printf("add th %d\n",sum);
+ return 0;
+}
