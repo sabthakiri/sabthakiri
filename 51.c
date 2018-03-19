5151f4486fb28bb8076f62652e438ae10dd3e56e
sabthakiri <35136081+sabthakiri@users.noreply.github.com>
2018-03-19 16:13:40 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int i,b ;
+	printf("enter the number \n");
+	scanf("%d",&b);
+ for(i=b; i>=1;i=0)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
